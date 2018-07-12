7d87acd1552d2cbca1d79d5ef1c94ac6f284ba03
根岸渉 <->
2018-07-12 21:51:13 +0900

    Revert "pl,motuika"
    This reverts commit 98660dc2e22d46225c93c5699b3ce1976efb5e52.

--- Team06/Actor/Player.cs
+++ Team06/Actor/Player.cs
@@ -4,19 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
-using Team06.Util;
-
 namespace Team06.Actor
 {
     class Player
     {
-        private Motion motion;
         private enum Direction
-        { DOWN, UP, RIGHT, LEFT };
+        {DOWN,UP,RIGHT,LEFT};
         private Direction direction;//現在の向き
-         //向きと範囲を管理
-        private Dictionary<Direction, Range> directionRange;
-
 
         private Vector2 position;//位置
         private string name;//画像の名前
@@ -25,28 +19,12 @@ namespace Team06.Actor
         public Player(string name)
         {
             this.name = name;
+            isDeadFlag = false;
+            position = Vector2.Zero;
         }
 
         public void Initialize()
-        {
-            position = Vector2.Zero;
-            isDeadFlag = false;
-
-            for (int i = 0; i < 16; i++)
-            {
-                motion.Add(i, new Rectangle(64 * (i % 4), 64 * (i / 4), 64, 64));
-            }
-
-            direction = Direction.DOWN;
-            directionRange = new Dictionary<Direction, Range>()
-            {
-
-                {Direction.DOWN,new Range(0,3) },
-                {Direction.UP,new Range(4,7) },
-                {Direction.RIGHT,new Range(8,11) },
-                {Direction.LEFT,new Range(12,15) },
-            };
-        }
+        { }
 
         public void Update(GameTime gameTime)
         {
